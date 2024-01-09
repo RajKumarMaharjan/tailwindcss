3c0024d0b0be8765aac3dfbabbc53f913631817a
Raj Kumar Maharja <rajkumarmaharjan006@gmail.com>
2024-01-09 09:15:18 +0545

    update and modify in responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,31 @@ module.exports = {
   content: ["*"],
   theme: {
     screens: {
+      'xs': '380px',
+
       'sm': '580px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '824px',
+      'md': '810px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      'lg': '992px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      'xl': '1026px',
       // => @media (min-width: 1290px) { ... }
 
-      '2xl': '1440px',
-      // => @media (min-width: 1536px) { ... }
+      '2xl': '1260px',
+      // // => @media (min-width: 1536px) { ... }
+      '3xl': '1440px',
+      // // => @media (min-width: 1536px) { ... }
     },
     extend: {
-      
+      lineHeight: {
+        'extra-loose': '14px',
+        'line': '100px',
+        '12': '3rem',
+      }
     },
   },
   plugins: [],
