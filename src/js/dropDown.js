20dbbf3f686737c6c719efc1f7e5eb833d94c331
Raj Kumar Maharja <rajkumarmaharjan006@gmail.com>
2024-01-10 18:00:44 +0545

    re-correct css property according to layout bases

---
+++ src/js/dropDown.js
@@ -0,0 +1,10 @@
+const dropdown = document.getElementById('dropdown')
+function toggleDropdown(){
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+
+};
+
+function closeDropdown(){
+    dropdown.style.display = "none"
+};
+
