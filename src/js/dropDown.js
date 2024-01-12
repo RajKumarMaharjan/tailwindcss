bf26a8122092d140877f1a43c2dc6298ea881d89
Raj Kumar Maharja <rajkumarmaharjan006@gmail.com>
2024-01-12 09:36:57 +0545

    use px % in h2 tag & business logo

--- src/js/dropDown.js
+++ src/js/dropDown.js
@@ -1,10 +1,23 @@
-const dropdown = document.getElementById('dropdown')
-function toggleDropdown(){
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+// const dropdown = document.getElementById('dropdown')
+// function toggleDropdown(){
+//     dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
 
-};
+// };
 
-function closeDropdown(){
-    dropdown.style.display = "none"
-};
+// function closeDropdown(){
+//     dropdown.style.display = "none"
+// };
 
+
+const menuItem = document.querySelector('#dropdown')
+const menuButton = document.querySelector('#mobile__menu')
+
+menuButton.addEventListener('click', () => {
+    menuItem.classList.toggle('hide')
+})
+
+document.addEventListener('click', e => {
+    if(!menuItem.contains(e.target) && e.target !== menuItem){
+        menuItem.classList.add('hide')
+    }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,32 +4,28 @@ module.exports = {
   theme: {
     screens: {
       'xxs': '320px',
-
+       // => @media (min-width: 320px) { ... }
       'xs': '375px',
-
+       // => @media (min-width: 375px) { ... }
       'sm': '540px',
-      // => @media (min-width: 640px) { ... }
-
+      // => @media (min-width: 540px) { ... }
       'md': '768px',
       // => @media (min-width: 768px) { ... }
-
       'lg': '912px',
-      // => @media (min-width: 1024px) { ... }
-
+      // => @media (min-width: 912px) { ... }
       'xl': '1024px',
-      // => @media (min-width: 1290px) { ... }
-
+       // => @media (min-width: 1024px) { ... }
       '2xl': '1280px',
-      // // => @media (min-width: 1536px) { ... }
+      // // => @media (min-width: 1280px) { ... }
       '3xl': '1440px',
-      // // => @media (min-width: 1536px) { ... }
+      // // => @media (min-width: 1440px) { ... }
     },
     extend: {
       lineHeight: {
         'extra-loose': '14px',
         'line': '100px',
         '12': '3rem',
-      }
+      },
     },
   },
   plugins: [],
